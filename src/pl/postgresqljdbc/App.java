d4528f492bb69b1d309fbaf07adc47aeaa7540bd
LazyBlazing <sniegowski.beniamin@gmail.com>
2022-04-16 17:16:36 +0200

    added Java program which connects to database and executes a query

---
+++ src/pl/postgresqljdbc/App.java
@@ -0,0 +1,46 @@
+package pl.postgresqljdbc;
+
+import java.sql.*;
+
+public class App {
+    private final String url = "jdbc:postgresql://localhost:5432/postgres"; // jdbc:postgresql://server-name:server-port/database-name
+    private final String user = "postgres";
+    private final String password = "";
+
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+    public static void printJans(ResultSet rs) throws SQLException {
+        while(rs.next()){
+            System.out.println(rs.getString("owner_id") + " " +
+                    rs.getString("adres") + " " +
+                    rs.getString("name") + " " +
+                    rs.getString("income")
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+//        app.connect();
+        String SQLquery = "SELECT *" +
+                "FROM owner_house JOIN houses USING (house_id) JOIN owners USING (owner_id) " +
+                "WHERE name='JAN'";
+        try(Connection conn = app.connect();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(SQLquery)) {
+            printJans(rs);
+        } catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
