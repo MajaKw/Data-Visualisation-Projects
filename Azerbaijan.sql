57b948a0fdaf8d92d10f715440cf8e9ac837372a
MajaKw <13maja.kwiatek@gmail.com>
2022-04-18 21:34:45 +0200

    update

--- Azerbaijan.sql
+++ Azerbaijan.sql
@@ -1,37 +1,38 @@
 CREATE TABLE Azerbaijan(
 	year numeric(4), children_out_of_school numeric(7),
 	battle_related_deaths numeric(7), 
-	government_expenditure_on_education_percentage numeric(2,1)
+	government_expenditure_on_education_percentage numeric(2,1),
+	GDP_per_capita numeric(2,1)
 );
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES(2000, 61173, NULL, 3.9);
+ government_expenditure_on_education_percentage) VALUES(2000, 61173, NULL, 3.9, 10.2);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2011, 49999, NULL, 2.4);
+ government_expenditure_on_education_percentage) VALUES (2011, 49999, NULL, 2.4, -1.2);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths,
- government_expenditure_on_education_percentage) VALUES (2012, 38511, 16,  2.1);
+ government_expenditure_on_education_percentage) VALUES (2012, 38511, 16,  2.1, 0.8);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2013, 8544, NULL, 2.4);
+ government_expenditure_on_education_percentage) VALUES (2013, 8544, NULL, 2.4, 4.5);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2014, 3638, 46, 2.6);
+ government_expenditure_on_education_percentage) VALUES (2014, 3638, 46, 2.6, 1.5);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2015, 5980, 63, 3.0);
+ government_expenditure_on_education_percentage) VALUES (2015, 5980, 63, 3.0, -0.1);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2016, 4338, 137, 2.9);
+ government_expenditure_on_education_percentage) VALUES (2016, 4338, 137, 2.9, -4.2);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2017, 12166, 36, 2.7);
+ government_expenditure_on_education_percentage) VALUES (2017, 12166, 36, 2.7, -0.8);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES(2018, 24803, NULL, 2.5);
+ government_expenditure_on_education_percentage) VALUES(2018, 24803, NULL, 2.5, 0.6);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2019, 55867, NULL, 2.7);
+ government_expenditure_on_education_percentage) VALUES (2019, 55867, NULL, 2.7, 1.6);
 
 INSERT INTO Azerbaijan(year, children_out_of_school, battle_related_deaths, 
- government_expenditure_on_education_percentage) VALUES (2020,  71122, 7623, 2.5);
+ government_expenditure_on_education_percentage) VALUES (2020,  71122, 7623, 2.5, -0.5);
